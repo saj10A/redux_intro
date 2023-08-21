c37f3bca4060a58bb5afab8fa0024e4c33f638f3
sajjad <sajjadnghd@gmail.com>
2023-08-21 11:05:38 +0330

    first-commit

---
+++ assets/js/app.js
@@ -0,0 +1,59 @@
+const fruitStates = {
+    apple: 5,
+    orange: 10
+}
+
+const foodStates = {
+    sandwich: 15
+}
+
+const fruitReducer = (state = fruitStates , action) => {
+    switch(action.type) {
+      case "BUY_APPLE":
+        return {...state, apple: state.apple - 1}
+      case "BUY_ORANGE":
+        return {...state, orange: state.orange - 1}  
+      default: 
+        return state;      
+    }
+}
+
+const foodReducer = (state = foodStates , action) => {
+    switch(action.type) {
+        case "BUY_SANDWICH": 
+          return {...state , sandwich: state.sandwich - 1}
+        default:
+          return state;    
+    }
+}
+
+const rootReducer = Redux.combineReducers({
+    fruit: fruitReducer,
+    food: foodReducer
+})
+
+const store = Redux.createStore(rootReducer);
+
+const render = () => {
+    document.getElementById("apple_count").innerHTML = store.getState().fruit.apple;
+    document.getElementById("orange_count").innerHTML = store.getState().fruit.orange;
+    document.getElementById("sandwich_count").innerHTML = store.getState().food.sandwich;
+}
+store.subscribe(() => {
+  render();
+})
+
+document.getElementById("buy_apple").addEventListener("click", () => {
+    store.dispatch({type: "BUY_APPLE"});
+})
+
+document.getElementById("buy_orange").addEventListener("click", () => {
+    store.dispatch({type: "BUY_ORANGE"});
+})
+
+document.getElementById("buy_sandwich").addEventListener("click", () => {
+    store.dispatch({type: "BUY_SANDWICH"});
+})
+
+render();
+
